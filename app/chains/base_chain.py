18aa163d5ff2d0139a54f9e22c8dcbdb2d1f29e8
konstantina <konstantinacs29@gmail.com>
2025-05-06 22:02:42 +0200

    feat/refactor: new empty file for base chain, moved gemini

--- app/chains/base_chain.py
+++ app/chains/base_chain.py
@@ -1,30 +1,14 @@
-from langchain.llms import BaseLLM
-from google import genai
-from typing import List, Dict, Any
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import os
+import json
+from typing import List
 
-class GeminiLLM(BaseLLM):
-    
-    def __init__(self, api_key: str, model: str = "gemini-2.0-flash"):
-        self.api_key = api_key
-        self.model = model
-        self.client = genai.Client(api_key=self.api_key)
+def main():
+    print("Generate new words for the user.")
+    # call gemini
+    print("Initialize user vocabulary.")
+    # update user vocabulary
 
-    def _call(self, prompt: str) -> str:
-        retries = 2
-        for _ in range(retries):
-            response = self.client.models.generate_content(model=self.model, contents=prompt)
-            try:
-                response_string = response.to_json_dict()["candidates"][0]["content"]["parts"][0]["text"]
-                response_string = response_string.replace("```", "").strip()
-            except (ValueError, KeyError, SyntaxError) as e:
-                print(f"Output parsing error: {e}. Retrying...")
-                continue
-        return response_string
-
-    @property
-    def _identifying_params(self) -> Dict[str, Any]:
-        return {"model": self.model}
-
-    @property
-    def _llm_type(self) -> str:
-        return "Google Gemini"
+if __name__ == "__main__":
+    main()
