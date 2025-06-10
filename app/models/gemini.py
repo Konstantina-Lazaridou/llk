75b68189baab1da181f068cb28bd45daff36f3c7
konstantina <konstantinacs29@gmail.com>
2025-06-10 17:42:14 +0200

    feat/fix: custom gemini class was buggy, replaced with built-in client

--- app/models/gemini.py
+++ app/models/gemini.py
@@ -1,34 +1,5 @@
-from langchain.llms import BaseLLM
-from google import genai
-import os
-from typing import List, Dict, Any, Optional
+from langchain.chat_models import init_chat_model
+from langchain_core.language_models.chat_models import BaseChatModel
 
-class GeminiLLM(BaseLLM):
-    
-    api_key: str = os.environ["GOOGLE_AI_STUDIO_API_KEY"]
-    model: str = "gemini-2.0-flash"
-    client: genai.Client = None
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        object.__setattr__(self, 'client', genai.Client(api_key=self.api_key))
-
-    def _generate(self, prompt: str) -> str:
-        retries = 2
-        for _ in range(retries):
-            response = self.client.models.generate_content(model=self.model, contents=prompt)
-            try:
-                response_string = response.to_json_dict()["candidates"][0]["content"]["parts"][0]["text"]
-                response_string = response_string.replace("```", "").strip()
-            except (ValueError, KeyError, SyntaxError) as e:
-                print(f"Output parsing error: {e}. Retrying...")
-                continue
-        return response_string
-
-    @property
-    def _identifying_params(self) -> Dict[str, Any]:
-        return {"model": self.model}
-
-    @property
-    def _llm_type(self) -> str:
-        return "Google Gemini"
+def get_gemini_model_client() -> BaseChatModel:
+    return init_chat_model("gemini-2.0-flash", model_provider="google_genai")
