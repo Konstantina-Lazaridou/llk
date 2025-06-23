4c01b1dac95db01f3953128f876398f6cdb18c62
konstantina <konstantinacs29@gmail.com>
2025-06-23 15:30:04 +0200

    feat: using env var for gemini version

--- langchain_app/models/gemini.py
+++ langchain_app/models/gemini.py
@@ -1,5 +1,6 @@
+import os
 from langchain.chat_models import init_chat_model
 from langchain_core.language_models.chat_models import BaseChatModel
 
 def get_gemini_model_client() -> BaseChatModel:
-    return init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+    return init_chat_model(model= os.environ["GEMINI_VERSION"], model_provider="google_genai")
